c6c05115bbdd769d6e7b9a0726fd6e49438a4476
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-21 20:51:42 +0000

    solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,15 @@
 def main():
     #write your code below this line
+    intList = []
+    max = 0
+    while(True):
+      number = int(input())
+      if(number==-1):
+        break
+      intList.append(number)
+      if(number>max):
+        max = number
+    print("The greatest number: " + str(max))
 
 if __name__ == '__main__':
     main()
